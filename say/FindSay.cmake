d4e58def1c60397447afa5690daf85b921c21f61
memsharded <james@conan.io>
2017-02-16 01:27:13 +0100

    basic find.cmake workign

---
+++ say/FindSay.cmake
@@ -0,0 +1,9 @@
+
+
+
+
+find_library(LIBSAY_LIBRARIES say.lib)
+
+find_path(LIBSAY_INCLUDE_DIRS say.h )
+
+#set(LIBSAY_INCLUDE_DIRS . )
--- say/conanfile.py
+++ say/conanfile.py
@@ -6,24 +6,16 @@ class SayConan(ConanFile):
     name = "say"
     version = "0.1"
     license = "MIT"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = ""
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     exports_sources = "*"
 
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line, ))
-        self.run("cmake --build . %s" % cmake.build_config)
-
-    def package(self):
-        self.copy("*.h", dst="include")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        install = '-DCMAKE_INSTALL_PREFIX="%s"' % self.package_folder
+        self.run('cmake %s %s %s' % (self.conanfile_directory, cmake.command_line, install))
+        self.run("cmake --build . --target install %s" % cmake.build_config)
 
     def package_info(self):
-        if self.scope.open:
-            self.cpp_info.rootpath = "../say"
-            self.cpp_info.includedirs = [""]
-            self.cpp_info.libdirs = ["../build/say/lib"]
         self.cpp_info.libs = ["say"]
